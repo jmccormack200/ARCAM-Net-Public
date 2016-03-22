7e1c64dfbac31e66574efa8923d5ca5b021982da
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-03-21 20:29:22 -0400

    Adding ZMQ

---
+++ ZMQ/hellozmq.py
@@ -0,0 +1,139 @@
+
+
+"""Decentralized chat example"""
+
+import argparse
+import os
+from threading import Thread
+
+from netifaces import interfaces, ifaddresses, AF_INET # dependency, not in stdlib
+
+import zmq
+
+class Node():
+    def __init__(IP,tun0,bat0):
+        self.IP = IP
+        self.tun0 = tun0
+        self.bat0 = bat0
+        self.name = str(self.IP) + "/" + str(self.tun0) + "/" + str(self.bat0) 
+
+class LocalNode(Node):
+
+    def __init__():
+        self.IP = parseIP()
+        self.tun0 = parseTun0()
+        self.bat0 = parseBat0()
+        self.name = str(self.IP) + "/" + str(self.tun0) + "/" + str(self.bat0) 
+
+    def parseIP():
+        addr = netifaces.ifaddresses('bat0')
+        self.IP = addr[netifaces.AF_INET][0]['addr']
+
+    def parseTun0():
+        addr = netifaces.ifaddresses('tun0')
+        self.tun0 = addr[netifaces.AF_LINK][0]['addr']
+
+    def parseBat0():
+        addr = netifaces.ifaddresses('bat0')
+        self.bat0 = addr[netifaces.AF_LINK][0]['addr']
+
+
+def listen(masked):
+    """listen for messages
+    
+    masked is the first three parts of an IP address:
+    
+        192.168.1
+    
+    The socket will connect to all of X.Y.Z.{1-254}.
+    """
+    ctx = zmq.Context.instance()
+    listener = ctx.socket(zmq.SUB)
+    for last in range(99, 110):
+        listener.connect("tcp://{0}.{1}:9001".format(masked, last))
+    
+    listener.setsockopt(zmq.SUBSCRIBE, b'')
+    while True:
+        try:
+            listenerOut = listener.recv_string()
+            print(listenerOut)
+            handleMsg(listenerOut)
+        except (KeyboardInterrupt, zmq.ContextTerminated):
+            break
+
+
+def handleMsg(msg):
+    #format    
+    #IP/tun0/bat0: freqChange rx/tx
+    #IP/tun0/bat0: OK null
+    #IP/tun0/bat0: heartbeat time
+    
+    #parse
+    msgParts = msg.split(' ')
+    sourceDat = msgParts[0].split('/')
+    msgType = msgParts[1]
+    msgDat = msgParts[2]
+
+    IP = sourceDat[0]
+    tun0 = sourceDat[1]
+    bat0 = sourceDat[2]
+
+    UpdateNodes(IP,tun0,bat0)
+
+    if msgType == 'freqChange':
+        print "Ip = " + str(IP)
+        print "tun0 = " + str(tun0)
+        print "bat0 = " + str(bat0)
+        print "msgDat = " + str(msgDat)
+    elif msgType == 'OK':
+        #updateChangeTable(IP)
+        pass
+    elif msgType == 'heartbeat':
+        #whatever  heartbeats do
+        pass
+    else:
+        print ("Invalid Message Type")
+`
+
+def UpdateNodes(IP,tun0,bat0):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("interface", type=str, help="the network interface",
+        choices=interfaces(),
+    )
+    parser.add_argument("user", type=str, default=os.environ['USER'],
+        nargs='?',
+        help="Your username",
+    )
+    args = parser.parse_args()
+    inet = ifaddresses(args.interface)[AF_INET]
+    addr = inet[0]['addr']
+    masked = addr.rsplit('.', 1)[0]
+    
+    ctx = zmq.Context.instance()
+    
+    listen_thread = Thread(target=listen, args=(masked,))
+    listen_thread.start()
+    
+    bcast = ctx.socket(zmq.PUB)
+    bcast.bind("tcp://%s:9001" % args.interface)
+    print("starting chat on %s:9001 (%s.*)" % (args.interface, masked))
+    localnode = LocalNode()
+    while True:
+        try:
+            msg = raw_input()
+            #IP/tun0/bat0: freqChange rx/tx
+            message = localnode.name + " freqChange 915000"
+            bcast.send_string(message)
+            #bcast.send_string("%s: %s" % (args.user, msg))
+        except KeyboardInterrupt:
+            break
+    bcast.close(linger=0)
+    ctx.term()
+
+if __name__ == '__main__':
+    main()
+
