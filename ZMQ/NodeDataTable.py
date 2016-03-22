43a6e22293291b929b829e60e25341e3cca60319
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-03-22 19:57:13 -0400

    changes to print

--- ZMQ/NodeDataTable.py
+++ ZMQ/NodeDataTable.py
@@ -50,7 +50,7 @@ class NodeDataTable():
 
     def printDict(self):
         for key in self.node_dict:
-            print(self.node_dict[key].__dict__)
+            print("{ '" + self.node_dict[key].IP + "' ," + " 'ACK: " + self.node_dict[key].ack + "}")
 
     def Flush(self):
         for key in self.node_dict:
