c46210db47a0a80612d6dc45a36e1c46679d2e54
John McCormack <mccormack.wgsi@gmail.com>
2016-04-02 15:40:06 -0400

    Updated UDPBatarang.py to be object oriented instead of a mess of global variables

---
+++ ZMQ/UDPbatarang.py
@@ -0,0 +1,224 @@
+
+
+"""Decentralized chat example"""
+# http://randalh.blogspot.ie/2012/12/zeromq-true-peer-connectivity-harmony.html
+
+import argparse
+import os
+from threading import Thread, Lock
+
+from Queue import Queue
+
+from netifaces import interfaces, ifaddresses, AF_INET, AF_LINK # dependency, not in stdlib
+
+import time
+
+import socket
+import select
+
+# Our Class Files
+from Node import Node
+from LocalNode import LocalNode
+from NodeDataTable import NodeDataTable
+
+import zmq
+
+
+class Batarang():
+
+    def __init__(self):
+        self.freqQue = Queue()
+
+        self.nodeDT = NodeDataTable()
+
+        self.localnode = LocalNode()
+
+        self.lock = Lock()
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("interface", type=str, help="the network interface",
+            choices=interfaces(),
+        )
+
+        self.args = parser.parse_args()
+        inet = ifaddresses(self.args.interface)[AF_INET]
+        addr = str(inet[0]['addr'])
+        self.masked = addr.rsplit('.', 1)[0]
+
+
+        pacemaker_thread = Thread(target=self.pacemaker, args=(addr,))
+        pacemaker_thread.daemon = True
+        pacemaker_thread.start()
+
+        self.freqQue_thread = Thread(target=self.freqChangeHandler, args=())
+        self.freqQue_thread.daemon = True
+        self.freqQue_thread.start()
+        
+        udp_thread = Thread(target=self.udprec, args=(addr,9000,))
+        udp_thread.daemon = True
+        udp_thread.start()
+
+        udp_thread_hb = Thread(target=self.udprec, args=(addr,9001,))
+        udp_thread_hb.daemon = True
+        udp_thread_hb.start()
+
+    def handleMsg(self, msg):
+        #format    
+        #IP/tun0/bat0: freqChange rx/tx time
+        #IP/tun0/bat0: OK rx/tx time
+        #IP/tun0/bat0: heartbeat rx/tx time
+        #parse
+
+        msgParts = msg.split(' ')
+        IP = msgParts[0]
+        msgType = msgParts[1]
+        msgDat = msgParts[2]
+        nodetime = msgParts[3]
+
+
+        #IP = sourceDat[0]
+        #tun0 = sourceDat[1]
+        #bat0 = sourceDat[2]
+
+        
+        newNode = Node(IP, msgDat, nodetime)
+
+        self.lock.acquire()
+
+        try:
+            if msgType == 'freqChange':
+                
+                #print "Ip = " + str(IP)
+                #print "tun0 = " + str(tun0)
+                #print "bat0 = " + str(bat0)
+                #print "msgDat = " + str(msgDat)
+                #print "time = " + str (time)
+                
+                
+                self.nodeDT.set_freq(msgDat)
+
+                
+                self.freqQue.put({"IP": IP,"msgDat": msgDat})
+
+            elif msgType == 'ACK':
+
+                self.nodeDT.ackNode(newNode)
+                self.nodeDT.printDict()
+
+            elif msgType == 'heartbeat':
+                self.nodeDT.rcvHeartbeat(newNode)
+                #print(IP[-3:] + " : " + str(round((float(time.time()) - float(nodetime)),3)))
+            else:
+                print ("Invalid Message Type")
+        finally:
+            self.lock.release()
+
+    def freqChangeHandler(self):
+        while True:
+            if not self.freqQue.empty():
+                data = self.freqQue.get()
+                IP = data['IP']
+                msgDat = data['msgDat']
+                print "Got request on Q IP = " + IP + " msg = " + msgDat
+                message = self.localnode.name + " ACK " + msgDat + " " + str(time.time())
+                
+                self.lock.acquire()
+                try:
+                    self.broadcastUDP(message,9000)
+                finally:
+                    self.lock.release()
+                
+                isfull = False
+                while not isfull:
+                    self.lock.acquire()
+                    try:
+                        isfull = self.nodeDT.checkIfTableIsFull()
+                    finally:
+                        self.lock.release()
+                
+                # Here we would send this data out over SocketIO
+                # Connect to socket
+                # Send to change,
+                print("Change success. Flushing table")
+                # Flush the table
+                self.lock.acquire()
+                try:
+                    self.nodeDT.Flush()
+                finally:       
+                    self.lock.release()
+
+                self.freqQue.task_done()
+
+    def broadcastUDP(self, msg, port=9000):
+        UDP_IP = '<broadcast>'
+        UDP_PORT = port
+
+        host='192.168.200.255'
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((host,0))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        #lock.acquire()
+        for a in range(0,10):
+            try:
+                sock.sendto(msg, (host, UDP_PORT))
+            finally:
+                pass
+        #        lock.release()  
+
+    def pacemaker(self, addr):
+        UDP_IP = '<broadcast>'
+        UDP_PORT = 9001
+
+        host='192.168.200.255'
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((host,0))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+         
+        while True:
+            msg =self.localnode.name + " heartbeat " + self.localnode.freq + " " + str(time.time())
+            #data = repr(str(addr[-3:]) + '\n')
+         #   lock.acquire()
+            try:
+                sock.sendto(msg, (host, UDP_PORT))
+            finally:
+          #      lock.release()
+                time.sleep(1)
+
+    def udprec(self, addr, port=9000):
+        UDP_PORT = port
+        bufferSize = 1024
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind(('', UDP_PORT))
+
+        while True:
+            msg, addr = sock.recvfrom(bufferSize)
+            print addr
+            self.handleMsg(msg)
+
+    def start(self):
+
+        print("starting chat on %s:9000 (%s.*)" % (self.args.interface, self.masked))
+
+        
+
+        while True:
+            try:
+                msg = raw_input()
+
+                message = self.localnode.name + " freqChange 915000 " + str(time.time())
+                self.lock.acquire()
+                try:
+                    self.broadcastUDP(message)
+                finally:
+                    self.lock.release()
+            except KeyboardInterrupt:
+                break
+
+if __name__ == '__main__':
+    batarang = Batarang()
+    batarang.start()
+
